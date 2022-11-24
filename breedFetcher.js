8b2c9aec60e019f09e70f39583383b0475f33c1d
ChunyuBai <baichunyu26@gmail.com>
2022-11-24 15:05:30 -0500

    create a cat librarythat user can play on it

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const catName = process.argv[2];
+const url = "https://api.thecatapi.com/v1/breeds/search?q=" + catName;
+request(url,(error,response,body) => {
+  const data = JSON.parse(body);
+  if(error){
+    console.log(error);
+    return;
+  }
+  if(!catName){
+    console.log(`please enter a cat name`);
+    return;
+  }
+  if(!data[0]){
+    console.log(`please enter a correct cat name`);
+    return;
+  }
+  // if(body.name !== catName || body.id !== catName){
+  //   console.log(`please enter a correct cat name`,error);
+  // }
+  console.log(data[0].description);
+  // console.log(typeof data);
+})
